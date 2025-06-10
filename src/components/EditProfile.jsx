3e9122fcb155f2b74f2ae60168443eba4c6845f0
Jaffar43 <mr.jaffarali2001@gmail.com>
2025-06-10 13:08:40 +0300

    add updateUserProfile in the services User and create EditProfile in components

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { updateUserProfile } from "../services/User"
+
+const EditProfile = () => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditProfile
